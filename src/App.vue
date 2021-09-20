bcd29d2e1ca3800bf7928cfd4c182bb678da43ac
po5ept <megastorm87@mail.ru>
2021-09-20 16:11:15 +0300

    Start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ hello }}</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'App',
+    data() {
+        return {
+            hello: 'Hello world',
+        }
+    }
+});
+</script>
+
+<style>
+</style>
