082501db710357e25dfaf76b3f31bc17c9fe51c4
test <jambackie@gmail.com>
2021-09-22 17:42:18 +0300

    рейтинг

---
+++ src/components/Rate.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="rating" :class="classes()" @click="clickOnStar">
+    <div id="1" class="rating__star"></div>
+    <div id="2" class="rating__star"></div>
+    <div id="3" class="rating__star"></div>
+    <div id="4" class="rating__star"></div>
+    <div id="5" class="rating__star"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Rate",
+  data() {
+    return {
+      star: "",
+    };
+  },
+
+  methods: {
+    classes() {
+      return `rating${this.star}`;
+    },
+
+    clickOnStar(event) {
+      this.star = event.target.id || "";
+    },
+  },
+});
+</script>
--- src/pages/login/LoginPage.vue
+++ src/pages/login/LoginPage.vue
@@ -1,32 +1,47 @@
 <template>
+  <div>
+    <modal-dialog v-model:is-open="modal">
+      <template #body>
+        <rate />
+      </template>
+    </modal-dialog>
     <button class="btn border-gradient">Играть как гость</button>
-    <span class="line-heading">Войти через соцсети</span>
-    <div class="society">
-        <div class="society__item vk"></div>
-        <div class="society__item fb"></div>
-        <div class="society__item gl"></div>
-    </div>
-    <span class="line-heading">Войти через Gamezz id</span>
+  <span class="line-heading">Войти через соцсети</span>
+  <div class="society">
+    <div class="society__item vk"></div>
+    <div class="society__item fb"></div>
+    <div class="society__item gl"></div>
+  </div>
+  <span class="line-heading">Войти через Gamezz id</span>
 
-    <login-form/>
+  <login-form />
 
-    <span class="line-heading">Еще нет аккаунта?</span>
-    <button class="btn enter__btn" @click="toRegisterPage">Зарегистрироваться</button>
+  <span class="line-heading">Еще нет аккаунта?</span>
+  <button class="btn enter__btn" @click="toRegisterPage">
+    Зарегистрироваться
+  </button>
+  </div>
 </template>
 
 <script>
-import {defineComponent} from 'vue';
-import LoginForm from './components/LoginForm.vue';
+import { defineComponent } from "vue";
+import LoginForm from "./components/LoginForm.vue";
+import ModalDialog from "../../components/modal-dialog/ModalDialog.vue"
+import Rate from "../../components/Rate.vue"
 
 export default defineComponent({
-    name: 'LoginPage',
-    inject: ['toRegisterPage'],
-    components: {
-        LoginForm,
-    },
-    data() {
-        return {};
-    },
+  name: "LoginPage",
+  inject: ["toRegisterPage"],
+  components: {
+    LoginForm,
+    ModalDialog,
+    Rate,
+  },
+  data() {
+    return {
+      modal: true,
+    };
+  },
 });
 </script>
 <style src="./css/login.css"></style>
