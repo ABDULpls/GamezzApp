2c0488b708ddf53088971374ba49181f2a0a6cf8
po5ept <megastorm87@mail.ru>
2021-09-24 11:20:26 +0300

    add UserWidget component

---
+++ src/components/ui/UserWidget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="miniprofile miniprofile-fix">
+        <span class="miniprofile__level">{{ user.level }}</span>
+        <img class="miniprofile__icon"
+             :src="user.avatar"
+             alt="pic"
+             width="35"
+             height="35">
+        <span class="miniprofile__results stars">{{ normalizeNumber(user.rating) }}</span>
+        <span class="miniprofile__results crystals">{{ normalizeNumber(user.crystals) }}</span>
+    </div>
+</template>
+
+<script>
+import {normalizeNumber} from '../../utils/utils';
+export default {
+    name: 'UserWidget',
+    props: {
+        user: {
+            type: Object,
+            required: true,
+        }
+    },
+    methods: {
+        normalizeNumber,
+    }
+};
+</script>
