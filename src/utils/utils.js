2a64deded00a6dadf84221b0297f06fa56229519
po5ept <megastorm87@mail.ru>
2021-09-24 11:19:58 +0300

    add helper function shuffle()

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-export const normalizeNumber = (number) => {
+export const normalizeNumber = number => {
     let sing = '';
 
     while(number > 1000) {
@@ -7,4 +7,11 @@ export const normalizeNumber = (number) => {
     }
 
     return Math.round(number * 10) / 10 + sing;
+}
+
+export const shuffle = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
