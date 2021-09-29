46e10c07de69e9e72214b89470cb839244c67520
po5ept <megastorm87@mail.ru>
2021-09-29 11:17:48 +0300

    add ui component BaseButton.vue

---
+++ src/components/ui/button/BaseButton.vue
@@ -0,0 +1,21 @@
+<script>
+import {h} from 'vue';
+
+const allowTags = ['a', 'button', 'span', 'div'];
+const defaultTag = 'button';
+
+export default {
+    name: 'BaseButton',
+    props: {
+        tag: {
+            type: String,
+            default: defaultTag,
+        }
+    },
+    render() {
+        const tag = allowTags.includes(this.tag) ? this.tag : defaultTag;
+
+        return h(tag, {}, this.$slots);
+    }
+};
+</script>
